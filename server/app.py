0fd29bc3a8e5bd750c53661084a83625e555924b
bran259 <kiprop686@gmail.com>
2026-01-11 14:47:47 +0300

    Authenticating Users

--- server/app.py
+++ server/app.py
@@ -47,10 +47,47 @@ class ShowArticle(Resource):
             return make_response(article_json, 200)
 
         return {'message': 'Maximum pageview limit reached'}, 401
+    
+class Login(Resource):
+
+    def post(self):
+        username = request.json.get('username')
+
+        user = User.query.filter(User.username == username).first()
+
+        if user:
+            session['user_id'] = user.id
+            return user.to_dict(), 200
+
+        return {}, 401
+    
+class Logout(Resource):
+
+    def delete(self):
+        session.pop('user_id', None)
+        return {}, 204
+    
+class CheckSession(Resource):
+
+    def get(self):
+        user_id = session.get('user_id')
+
+        if user_id:
+            user = User.query.get(user_id)
+            return user.to_dict(), 200
+
+        return {}, 401
+
+
+
 
 api.add_resource(ClearSession, '/clear')
 api.add_resource(IndexArticle, '/articles')
 api.add_resource(ShowArticle, '/articles/<int:id>')
+api.add_resource(Login, '/login')
+api.add_resource(Logout, '/logout')
+api.add_resource(CheckSession, '/check_session')
+
 
 
 if __name__ == '__main__':
